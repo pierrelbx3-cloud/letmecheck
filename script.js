b6d354ab6b0fbb554c8b01705856a9031225a716
pierrelbx3-cloud <pierrelbx3@gmail.com>
2025-11-11 10:12:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 
 // Initialisation du client Supabase
 // Ceci crée une instance que nous utiliserons pour toutes les communications API.
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 
 // =================================================================
